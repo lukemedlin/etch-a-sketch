4bc221f04625b5712111642d3b70841ee195eae5
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-01-24 22:31:42 -0600

    Create new grid button and logic

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const main = document.querySelector("main");
+document.querySelector("button").addEventListener("click", newGrid);
+let boxAmount = 4;
 
+createGrid();
 
-let boxAmount = 10;
+function newGrid() {
+    boxAmount = prompt("Enter grid size");
 
+    if (boxAmount > 100) {
+        alert("Enter a number below 100");
+        return newGrid();
+    }
 
-for (let i = 0; i < boxAmount * boxAmount; i++) {
-    const block = document.createElement("div");
-    block.style.width = `${960 / boxAmount}px`;
-    block.style.height = `${960 / boxAmount}px`;
-    block.addEventListener("mouseover", () => {
-        block.classList.add("hovered")
-    }); 
-    main.appendChild(block);
-
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+    createGrid();
+    
 }
 
+function createGrid() {
+    for (let i = 0; i < boxAmount * boxAmount; i++) {
+        const block = document.createElement("div");
+        block.style.width = `${960 / boxAmount}px`;
+        block.style.height = `${960 / boxAmount}px`;
+        block.addEventListener("mouseover", () => {
+            block.classList.add("hovered")
+        }); 
+        main.appendChild(block);
+    }
+}
